2bc0261c0aa7246886ea49bbdd4add4b9c14b617
BrahmantyaWardhana <brahmantya.wardhana.billy@gmail.com>
2026-03-20 20:30:48 +0700

    add: gin init

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import "github.com/gin-gonic/gin"
 
 func main() {
-	fmt.Println("Hello, World!")
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	router.Run() // listens on 0.0.0.0:8080 by default
 }
