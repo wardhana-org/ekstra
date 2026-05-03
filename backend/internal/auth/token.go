372baa3ba1599c75d6be03cdd9e0a45045b8cdde
BrahmantyaWardhana <brahmantya.wardhana.billy@gmail.com>
2026-05-03 12:26:22 +0700

    add: created GenerateRawToken and HashToken function in token.go

--- backend/internal/auth/token.go
+++ backend/internal/auth/token.go
@@ -2,6 +2,9 @@ package auth
 
 import (
 	"crypto/rand"
+	"crypto/sha256"
+	"encoding/base64"
+	"encoding/hex"
 )
 
 func randomBytes(length uint32) ([]byte, error) {
@@ -11,4 +14,18 @@ func randomBytes(length uint32) ([]byte, error) {
 	}
 
 	return bytes, nil
+}
+
+func GenerateRawToken() (string, error) {
+	bytes, err := randomBytes(32)
+	if err != nil {
+		return "", err
+	}
+
+	return base64.RawURLEncoding.EncodeToString(bytes), nil
+}
+
+func HashToken(rawToken string) string {
+	hashedToken := sha256.Sum256([]byte(rawToken))
+	return hex.EncodeToString(hashedToken[:])
 }
