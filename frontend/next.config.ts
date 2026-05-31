83e89d507d90a578fc1912168011143300592a7c
BrahmantyaWardhana <brahmantya.wardhana.billy@gmail.com>
2026-05-31 15:42:42 +0700

    update: used official nextjs "standalone" docker deployment setup pattern

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const backendApiUrl = process.env.BACKEND_API_URL ?? "http://localhost:8080";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   async rewrites() {
     return [
       {
