568cd9e2026dba84c68f57a5ba4c01621a997508
BrahmantyaWardhana <brahmantya.wardhana.billy@gmail.com>
2026-06-14 15:31:25 +0700

    update: remove auth placeholder and cleaned up docker for dev

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,11 +1,7 @@
-import { LoginTester } from "@/features/auth/components/LoginTester";
-
 export default function Home() {
   return (
     <main>
       <h1>Ekstra</h1>
-      <p>Backend auth test.</p>
-      <LoginTester />
     </main>
   );
 }
